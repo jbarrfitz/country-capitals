f8c93985395aa05efe9a59c1f085fe4353a96830
Jerry Barrows-Fitzgerald <jbarrowsfitzgerald@gmail.com>
2023-05-10 21:56:46 -0400

    feat: implemented search by country

---
+++ api/capital-finder.py
@@ -0,0 +1,35 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+        capital_city = ""
+        country_name = ""
+
+        if "name" in dic:
+            url = "https://restcountries.com/v3.1/name/"
+            country_req = requests.get(url + dic["name"])
+            country_data = country_req.json()
+            capital_city = country_data[0]["capital"][0]
+        if "capital" in dic:
+            url = "https://restcounrtires.com/v3.1/capital/"
+            capital_req = requests.get(url + dic["capital"])
+            capital_data = capital_req.json()
+            country_name = capital_data[0]["name"]["common"]
+
+        if capital_city:
+            message = f"The capital of {dic['name']} is {capital_city}."
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+        self.wfile.write(message.encode())
+
+        return
